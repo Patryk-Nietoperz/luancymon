4a1224074219d0033bd981b40ef6d02e0d32e475
Patryk-Nietoperz <noworyckidawid@gmail.com>
2026-04-21 17:17:59 +0200

    coś tam naszponcilem (plik 5 i 6)

---
+++ plik#5.lua
@@ -0,0 +1,23 @@
+local liczba1 = 4
+local liczba2 = 9
+
+print(liczba1+liczba2)
+print(liczba1-liczba2)
+print(liczba1*liczba2)
+print(liczba1/liczba2)
+print(liczba1%liczba2)
+print(liczba1^liczba2)
+
+
+local c = liczba1 ~= liczba2
+if c == true then
+    print("jes git")
+else
+    print("nie jest g")
+end
+
+if liczba1 == 2 and liczba2 == 2 then
+    print("jest g")
+else
+    print("nie jest git")
+end
---
+++ plik#6.lua
@@ -0,0 +1,22 @@
+for kuta=1,10 do
+    local p = 'siusia'
+    for s=1,10,2 do
+        print(p..'k')
+    end
+end
+
+
+local lista = {'z', 'x', 'c', 'v', 'b'}
+for i=1, #lista do
+    print(lista[i])
+end
+
+
+local a = 10
+while a <=67 do
+    if a >= 41 then
+        break
+    end
+    print("a ma wartosc: "..a)
+    a=a+6.67
+end
